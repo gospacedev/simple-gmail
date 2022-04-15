41428f099b65d2314ea7dca8ab39c14a61aeacc7
GOCrazy7575 <83633399+gocrazygh@users.noreply.github.com>
2022-04-15 15:51:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+sender_email = input("Enter sender's gmail: ")
+password = input("Enter sender's password: ")
+receiver_email = input("Enter recevier's gmail: ")
+subject = input("Enter email subject: ")
+body = input("Enter email body: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
+
+print("Sending Email!")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+print("Success")
